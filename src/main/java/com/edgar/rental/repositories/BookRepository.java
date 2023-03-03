edc76a8dae668095da83a619ac8545a85f115b32
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-03-02 23:58:10 -0500

    repo

--- src/main/java/com/edgar/rental/repositories/BookRepository.java
+++ src/main/java/com/edgar/rental/repositories/BookRepository.java
@@ -1,9 +1,11 @@
 package com.edgar.rental.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.edgar.rental.models.Book;
 
-public interface BookRepository  extends JpaRepository<Book, Long>{
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
--- src/main/java/com/edgar/rental/repositories/RentRepository.java
+++ src/main/java/com/edgar/rental/repositories/RentRepository.java
@@ -1,9 +1,11 @@
 package com.edgar.rental.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.edgar.rental.models.Rent;
 
-public interface RentRepository  extends JpaRepository<Rent, Long>{
+@Repository
+public interface RentRepository extends JpaRepository<Rent, Long> {
 
 }
